ed8d998a05200b8021162e2de830a534ac122a39
jvviel <p-vittor@hotmail.com>
2018-10-29 16:02:07 -0300

    script banco

---
+++ script-banco.sql
@@ -0,0 +1,25 @@
+create table esportes (
+id bigint(20) primary key auto_increment,
+descricao varchar(50));
+
+create table ligas(
+id bigint(20) primary key auto_increment,
+descricao varchar(50),
+pais varchar(50),
+esporte bigint(20),
+premio varchar(50),
+foreign key(esporte) references esportes(id));
+
+create table times (
+id bigint(20) primary key auto_increment,
+descricao varchar(50),
+liga bigint(20),
+tecnico varchar(50),
+estadio varchar(50),
+foreign key(liga) references ligas(id));
+
+create table jogadores (
+id bigint(20) primary key auto_increment,
+nome varchar(50),
+time bigint(20),
+foreign key(time) references times(id));
